86bc0bca2dc96ce0ba6a6d8aa3610eefd208c1b0
Lasya2104 <24B11DS217@adityauniversity.in>
2026-01-26 18:28:55 +0530

    Time: 4 ms (10.99%), Space: 11.9 MB (11.58%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
+    void fun(vector<int>nums, vector<int>&ds, vector<vector<int>>&ans, int frq[])
+    {
+        if(ds.size() == nums.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(!frq[i])
+            {
+                ds.push_back(nums[i]);
+                frq[i]=1;
+                fun(nums,ds,ans,frq);
+                frq[i]=0;
+                ds.pop_back();
+            }
+        }
+    }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>res;
-        sort(nums.begin(),nums.end());
-        do{
-            res.push_back(nums);
-        }while(next_permutation(nums.begin(),nums.end()));
-        return res;
+        vector<vector<int>>ans;
+        vector<int>ds;
+        int frq[nums.size()];
+        for(int i=0;i<nums.size();i++)
+        {
+            frq[i]=0;
+        }
+        fun(nums,ds,ans,frq);
+        return ans;
     }
 };
