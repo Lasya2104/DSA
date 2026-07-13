3f881df9b2a0ff9e42a3280c220745bcc4d9bb00
Lasya2104 <24B11DS217@adityauniversity.in>
2026-07-13 06:39:57 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.4%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,36 +1,28 @@
-bool binary_search(vector<int>arr,int value)
-{
-    int low=0,high=arr.size()-1;
-    while(low<=high)
-    {
-        int mid=(low+high)/2;
-        if(arr[mid]==value)
-        {
-            return true;
-        }
-        else if(arr[mid]<value)
-        {
-            low=mid+1;
-        }
-        else
-        {
-            high=mid-1;
-        }
-    }
-    return false;
-}
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        for(int i=0;i<n;i++)
+        int idx=0;
+        for(int i=0;i<matrix.size();i++)
         {
-            if(target>=matrix[i][0] && target<=matrix[i][m-1])
+            if(target>=matrix[i][0])
             {
-                return binary_search(matrix[i],target);
+                idx=i;
+            }
+            else
+            {
+                break;
             }
         }
-        return 0;
+        cout<<idx;
+        int flag=0;
+        for(int i=0;i<matrix[idx].size();i++)
+        {
+            if(find(matrix[idx].begin(),matrix[idx].end(),target)!=matrix[idx].end())
+            {
+                flag=1;
+            }
+        }
+        if(flag==0)return false;
+        else return true;
     }
 };
