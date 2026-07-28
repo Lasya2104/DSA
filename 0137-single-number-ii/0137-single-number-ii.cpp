0af32cf2d6d027cc161571d66d969bc7a84dc085
Lasya2104 <24B11DS217@adityauniversity.in>
2026-07-28 21:15:08 +0530

    Time: 3 ms (31.4%), Space: 14.1 MB (11.17%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>frq;
+        for(int i=0;i<nums.size();i++)
+        {
+            frq[nums[i]]++;
+        }
+        int res=0;
+        for(auto &p:frq)
+        {
+            if(p.second==1)res=p.first;
+        }
+        return res;
+    }
+};
