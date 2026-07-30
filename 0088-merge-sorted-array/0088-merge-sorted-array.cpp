fbab7d1196f72b76e4ea52b9e8f2556beb29666f
Lasya2104 <24B11DS217@adityauniversity.in>
2026-07-30 07:00:07 +0530

    Time: 0 ms (100%), Space: 12.3 MB (55.6%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         nums1.resize(m+n);
-        int j=0;
-        for(int i=m;i<(m+n);i++)
+        int k=0;
+        for(int i=m;i<nums1.size();i++)
         {
-            nums1[i]=nums2[j++];
+            nums1[i]=nums2[k++];
         }
         sort(nums1.begin(),nums1.end());
     }
