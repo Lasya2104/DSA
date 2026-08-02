ddd9f2edb81c3c868f19c4a7be0e1e4bd749c490
Lasya2104 <24B11DS217@adityauniversity.in>
2026-08-02 17:55:40 +0530

    Time: 0 ms (100%), Space: 10.4 MB (68.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int esum=0,osum=0;
+        for(int i=0;i<piles.size();i++)
+        {
+            if(i%2==0)esum+=piles[i];
+            else osum+=piles[i];
+        }
+        if(esum>osum || osum>esum)return true;
+        else return false;
     }
 };
