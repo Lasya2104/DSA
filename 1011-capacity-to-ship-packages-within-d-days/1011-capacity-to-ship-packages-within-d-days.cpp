01033df4a7c9830bb4b72b673e528bfb52d68ea1
Lasya2104 <lasyaveepuri@gmail.com>
2026-09-01 15:04:50 +0530

    Time: 22 ms (5.96%), Space: 60.7 MB (19.83%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,42 +1,32 @@
 class Solution {
 public:
-    int dayreq(vector<int>s,int cap)
-    {
-        int day=1,load=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(load+s[i]>cap)
-            {
+    int dayreq(vector<int> s, int cap) {
+        int day = 1, load = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (load + s[i] > cap) {
                 day++;
-                load=s[i];
-            }
-            else
-            {
-                load+=s[i];
+                load = s[i];
+            } else {
+                load += s[i];
             }
         }
         return day;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int maxi=*max_element(weights.begin(),weights.end());
-        int sum=0;
-        for(int i=0;i<weights.size();i++)
-        {
-            sum+=weights[i];
+        int maxi = *max_element(weights.begin(), weights.end());
+        int sum = 0;
+        for (int i = 0; i < weights.size(); i++) {
+            sum += weights[i];
         }
-        int mini=INT_MAX;
-        int low=maxi,high=sum;
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if(dayreq(weights,mid)<=days)
-            {
-                mini=mid;
-                high=mid-1;
-            }
-            else
-            {
-                low=mid+1;
+        int mini = INT_MAX;
+        int low = maxi, high = sum;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (dayreq(weights, mid) <= days) {
+                mini = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return mini;
