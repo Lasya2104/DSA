85d0f8cfc06034a4fb1c22b351565cc4243651c8
Lasya2104 <lasyaveepuri@gmail.com>
2026-09-26 07:35:21 +0530

    Time: 0 ms (100%), Space: 12.5 MB (20.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //The tc is o(n log n)
+        //The sc is o(1)
         nums1.resize(m+n);
         int k=0;
         for(int i=m;i<nums1.size();i++)
